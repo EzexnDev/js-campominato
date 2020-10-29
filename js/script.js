203b2a3f496bca78f591a7e0628f6f2cb960530f
Gianmarco <gianmarco.cavalieredev@gmail.com>
2020-10-29 18:31:42 +0100

    completato con bonus 1

--- js/script.js
+++ js/script.js
@@ -1,47 +1,106 @@
+//Functions
 
+function getRandom(min,max){
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-function randomPcNumbers(max){
-    var arr=[];
-    var length = 16;
-    console.log('qua ce vita');
-    for (var i = 0 ; i<length ; i++){
-        var num = Math.floor(Math.random()*(100-1) + 1)
-        check(arr,num);
+function isPresent(array, num){
+  return array.indexOf(num) != -1;
+}
+
+function isValidNumber(number,min,max){
+  return !isNaN(number) && number >= min && number <= max;
+}
+
+function start(){
+  var level;
+  var level_check = true;
+  var max_range;
+  var user_num_array = [];
+  var num_array = [];
+  var max_pc_numbers = 16;
+  var score = 0;
+  
+  //Execution
+  do{
+
+    var level_check = false;
+    level = prompt("Inserisci un livello di difficoltà : 0 - 1 - 2");
+
+    switch(level){
+      case "0":
+          max_range = 100;
+          break;
+
+      case "1":
+          max_range = 80;
+          break;
+
+      case "2":
+          max_range = 50;
+          break;
+
+      default:
+          level_check = true;
+          alert("Il livello inserito non è disponibile, riprova!");
+          break;
     }
-    return arr;
-}
+  }while(level_check);
 
-function userNumbers(max,pcarray){
-    var arr=[];
-    var lengthUser = max- pcarray.length;
-    for (var i = 0 ; i<lengthUser ; i++){
-        console.log('entro')
-        var num = prompt("Inserisci Numero");
-        check(arr,num);
-        for(var n=0; n<pcarray.length; n++){
-            if(arr[i]==pcarray[n]){
-                console.log("hai perso");
-                return message="Hai perso"
-            }
-        }
+  console.log("Difficoltà inserita : " + level);
+  console.log("Genero " + max_pc_numbers + " numeri computer");
+
+  var random_count = 0;
+
+  while(random_count < 16){
+    var rand_num = getRandom(1,max_range);
+    if(!isPresent(num_array,rand_num)){
+      num_array.push(rand_num);
+      random_count++;
     }
-    console.log('Hai vinto!')
-    return message="Hai Vinto"
-}
+  }
 
-function check(arr,num){
-    if (arr.indexOf(num) === -1) {
-        arr.push(num)
-    };
-}
+  console.log("Numeri del computer : \n" + num_array);
 
-function game(){
+  console.log("Inizio input utente");
+
+  var continue_game = true;
+  var user_message;
+
+  while(continue_game){
+
+    var n = prompt("Inserisci un numero compreso tra 1 e " + max_range);
+    var input = parseInt(n);
     
+    if(!isValidNumber(input,1,max_range)){
+      alert("L'input inserito non è corretto, riprova!");
+      continue;
+    }
+
+    if(isPresent(user_num_array,input)){
+      alert("Hai già inserito questo numero, riprova!");
+      continue;
+    }
+    user_num_array.push(input);
+
+    if(isPresent(num_array,input)){
+      user_message = "Hai inserito un numero scelto dal computer! HAI PERSO! Punteggio " + score;
+      continue_game = false;
+      break;
+    }
+
+    console.log("Complimenti! + 1 punto");
+    score++;
+
+    if(user_num_array.length == (max_range - max_pc_numbers)){
+      user_message = "Hai Vinto! Bravo! Punteggio " + score;
+      continue_game = false;
+    }
+
+    console.log("Numeri dell'utente : \n" + user_num_array);
+  }
+
+  alert(user_message);
 }
 
-
-var pcArray = randomPcNumbers(100);
-console.log(pcArray);
-userNumbers(100,pcArray);
-
-
+start();
